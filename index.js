143bb3ff1a119544c1e85941fa7d1d1e03f9044c
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-11-08 02:53:24 +0600

    Thrid Commit For Update Route

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const services = require('./data/services.json');
+const users = require('./data/users.json');
+
+app.get('/services', (req, res) => {
+    res.send(services);
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
 app.get('/', (req, res) => {
     res.send('Services Reviews is Running');
 })
